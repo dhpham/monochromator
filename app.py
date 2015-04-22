fa6e7b5ae4c390d4a038f0f664f419a7f57bf679
dhpham <don.pham@utexas.edu>
2015-04-21 22:08:22 -0500

    initial commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, jsonify, render_template
+
+app = Flask(__name__)
+
+@app.route('/', methods = ['GET'])
+def index():
+    return render_template('index.html')
+
+@app.route('/text', methods = ['POST'])
+def hello():
+    text = request.form['textbox']
+    return text
+
+
+
+if __name__ == "__main__":
+    app.run(host = "0.0.0.0", debug = True)
