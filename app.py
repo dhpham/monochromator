d547eae8765120378689554dd6857598df41cac4
dhpham <don.pham@utexas.edu>
2015-04-21 23:46:45 -0500

    refresh

--- app.py
+++ app.py
@@ -13,9 +13,7 @@ def hello():
 
 @app.route('/refresh', methods = ['GET'])
 def refresh():
-    return render_template('refresh.html')
-
-
+    return render_template('refresh.html', f=1)
 
 if __name__ == "__main__":
     app.run(host = "0.0.0.0", port = 5000, debug = True)
