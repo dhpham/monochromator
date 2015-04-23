a92822fd9b1dae034672e1fed0b2d04b772776fc
dhpham <don.pham@utexas.edu>
2015-04-23 09:53:13 -0500

    added control

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify
 
 app = Flask(__name__)
 
@@ -14,6 +14,25 @@ def hello():
 @app.route('/refresh', methods = ['GET'])
 def refresh():
     return render_template('refresh.html', f=1)
+
+@app.route('/control', methods = ['GET'])
+def control():
+    return render_template('control.html')
+
+@app.route('/stepper', methods = ['POST'])
+def stepper():
+    steps = int(request.form['steps'])
+    delay = float(request.form['delay'])
+    direction = str(request.form['dir'])
+    microstep = int(request.form['ms'])
+
+    r = {}
+    r['steps'] = steps
+    r['delay'] = delay
+    r['direction'] = direction
+    r['microstep'] = microstep
+
+    return jsonify(r)
 
 if __name__ == "__main__":
     app.run(host = "0.0.0.0", port = 5000, debug = True)
