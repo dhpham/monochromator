41d2ce39754a827a9ca8febb99391abe52b2ff85
dhpham <don.pham@utexas.edu>
2015-04-30 23:51:16 -0500

    routes

--- app.py
+++ app.py
@@ -11,10 +11,6 @@ def index():
 def control():
     return render_template('control.html')
 
-@app.route('/stepper', methods = ['POST'])
-def stepper():
-    # return redirect('/do_steps', code=307)
-
 @app.route('/do_steps', methods = ['POST'])
 def do_steps():
     steps = int(request.form['steps'])
@@ -26,27 +22,30 @@ def do_steps():
 
     r = {}
     r['steps'] = steps
-    r['delay'] = delay
     r['direction'] = direction
+    r['delay'] = delay
     r['microstep'] = microstep
 
     return jsonify(r)
 
 @app.route('/do_obs', methods = ['GET'])
 def do_obs():
-    pass
+    # main.do_Obs()
+    return True
 
 @app.route('/calibrate', methods = ['GET'])
 def calibrate():
-    pass
+    # main.calibrate()
+    return True
 
 @app.route('/spectrum', methods = ['GET'])
 def spectrum():
-    pass
+    return render_template('spectrum.html')
 
 @app.route('/go_home', methods = ['GET'])
 def go_home():
-    pass
+    # main.go_Home()
+    return True
 
 if __name__ == "__main__":
     app.run(host = "0.0.0.0", port = 5000, debug = True)
